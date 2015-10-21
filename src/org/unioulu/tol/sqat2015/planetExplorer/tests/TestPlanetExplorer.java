ec589befa982e04c034d87164d8bf364d36109d4
somename <someemail>
2015-10-21 15:00:47 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqat2015/planetExplorer/tests/TestPlanetExplorer.java
+++ src/org/unioulu/tol/sqat2015/planetExplorer/tests/TestPlanetExplorer.java
@@ -14,7 +14,7 @@ public class TestPlanetExplorer {
 	
 	@Test
 	public void testInitialStatusRequest() {
-		// Test returning the location of the explorer.
+		// Test returning the location and heading of the explorer.
 		PlanetExplorer command = new PlanetExplorer(100, 100, null);
 		assertEquals("(0,0,N)",command.executeCommand(""));
 	}
