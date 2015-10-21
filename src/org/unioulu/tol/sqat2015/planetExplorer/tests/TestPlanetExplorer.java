da56c76a35ea9be315d1d6d572c23210b870c310
somename <someemail>
2015-10-21 14:54:56 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqat2015/planetExplorer/tests/TestPlanetExplorer.java
+++ src/org/unioulu/tol/sqat2015/planetExplorer/tests/TestPlanetExplorer.java
@@ -8,6 +8,8 @@ public class TestPlanetExplorer {
 	
 	public static String validJourney = "ffrflb";
 	public static String invalidJourney = "fgblfr";
+	public static String validObstacle = "(0,1)(4,4)";
+	public static String invalidObastacle = "(0,a),(0,10)";
 	
 	@Test
 	public void test() {
