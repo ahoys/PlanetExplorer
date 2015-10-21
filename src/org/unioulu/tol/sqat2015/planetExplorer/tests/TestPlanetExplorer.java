f587366d18076af5c6865813eeb0d4127a01b576
somename <someemail>
2015-10-21 16:36:59 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqat2015/planetExplorer/tests/TestPlanetExplorer.java
+++ src/org/unioulu/tol/sqat2015/planetExplorer/tests/TestPlanetExplorer.java
@@ -23,13 +23,6 @@ public class TestPlanetExplorer {
 	}
 	
 	@Test
-	public void testInitialStatusRequest() {
-		// Test returning the location and heading of the explorer.
-		PlanetExplorer planet = new PlanetExplorer(100,100,null);
-		assertEquals("(0,0,N)",planet.executeCommand(""));
-	}
-	
-	@Test
 	public void testMoveForward() {
 		PlanetExplorer planet = new PlanetExplorer(100,100,null);
 		assertEquals("(0,0,N)",planet.executeCommand("f"));
