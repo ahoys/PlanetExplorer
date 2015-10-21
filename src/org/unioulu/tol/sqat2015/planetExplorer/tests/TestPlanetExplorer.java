20cd9281ef5346fe4ffd7ded723e69bd716f1565
somename <someemail>
2015-10-21 14:52:11 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqat2015/planetExplorer/tests/TestPlanetExplorer.java
+++ src/org/unioulu/tol/sqat2015/planetExplorer/tests/TestPlanetExplorer.java
@@ -5,7 +5,9 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class TestPlanetExplorer {
-
+	
+	public static String validJourney = "ffrflb";
+	
 	@Test
 	public void test() {
 		fail("Not yet implemented");
