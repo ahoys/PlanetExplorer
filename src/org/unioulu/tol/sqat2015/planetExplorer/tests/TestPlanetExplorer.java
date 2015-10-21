c3425ad0a345fb0bedf0a6d54bee6e4b01ad6ffb
somename <someemail>
2015-10-21 15:50:37 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqat2015/planetExplorer/tests/TestPlanetExplorer.java
+++ src/org/unioulu/tol/sqat2015/planetExplorer/tests/TestPlanetExplorer.java
@@ -10,38 +10,38 @@ public class TestPlanetExplorer {
 	@Test
 	public void testReturningPlanetSizeX(){
 		// Return the x-size limit as a int value.
-		PlanetExplorer planet1 = new PlanetExplorer(100,100,null);
-		assertEquals(100,planet1.getPlanetX());
+		PlanetExplorer planet = new PlanetExplorer(100,100,null);
+		assertEquals(100,planet.getPlanetX());
 	}
 	
 	@Test
 	public void testReturningPlanetSizeY(){
 		// Return the y-size limit as a int value.
-		PlanetExplorer planet2 = new PlanetExplorer(100,100,null);
-		assertEquals(100,planet2.getPlanetY());
+		PlanetExplorer planet = new PlanetExplorer(100,100,null);
+		assertEquals(100,planet.getPlanetY());
 	}
 	
 	@Test
 	public void testInitialStatusRequest() {
 		// Test returning the location and heading of the explorer.
-		PlanetExplorer planet3 = new PlanetExplorer(100,100,null);
-		assertEquals("(0,0,N)",planet3.executeCommand(""));
+		PlanetExplorer planet = new PlanetExplorer(100,100,null);
+		assertEquals("(1,0,N)",planet.executeCommand(""));
 	}
 	
 	@Test
 	public void testReturningNewExplorerXlocation() {
 		// Test returning the location and heading of the explorer.
-		PlanetExplorer planet4 = new PlanetExplorer(100,100,null);
-		planet4.setLocationX(1);
-		assertEquals(1,planet4.getLocationX());
+		PlanetExplorer planet = new PlanetExplorer(100,100,null);
+		planet.setLocationX(1);
+		assertEquals(1,planet.getLocationX());
 	}
 	
 	@Test
 	public void testReturningNewExplorerYlocation() {
 		// Test returning the location and heading of the explorer.
-		PlanetExplorer planet5 = new PlanetExplorer(100,100,null);
-		planet5.setLocationY(2);
-		assertEquals(2,planet5.getLocationY());
+		PlanetExplorer planet = new PlanetExplorer(100,100,null);
+		planet.setLocationY(2);
+		assertEquals(2,planet.getLocationY());
 	}
 	
 }
